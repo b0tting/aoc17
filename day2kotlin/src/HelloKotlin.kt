6ae28a0658334aa0f8a1206e84aadfed5dd249d5
b0tting <74Dollars>
2017-12-02 23:27:15 +0100

    Added kotlin solution, should kotlineer functions

---
+++ day2kotlin/src/HelloKotlin.kt
@@ -0,0 +1,43 @@
+package demo
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStream
+
+fun main(args : Array<String>) {
+    val lines = readFile("E:\\drive\\intellij\\aoc17\\day2\\input.txt")
+    println(getCheckSum(lines))
+
+}
+
+fun readFile(filename : String) : MutableList<List<Int>>{
+    val spreadsheet = mutableListOf<List<Int>>()
+    val bufferedReader: BufferedReader = File(filename).bufferedReader()
+    bufferedReader.useLines{lines->lines.forEach {
+        val currline = it.split("\t");
+        var currLineInt = currline.map {
+            it.toInt()
+        }
+        spreadsheet.add(currLineInt)
+    }}
+    return spreadsheet
+}
+
+fun getCheckSum(spreadsheet : List<List<Int>>) : Int {
+    var sum = 0
+    for (numbers in spreadsheet) {
+        var lowest = numbers[0]
+        var highest = lowest
+        for (number in numbers) {
+            if (number < lowest) {
+                lowest = number
+            }
+
+            if (number > highest) {
+                highest = number
+            }
+        }
+        sum += highest - lowest
+    }
+    return sum
+}
